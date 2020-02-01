fa685623d8327ea85869942aa51a69521dde92e5
Yutao Chen <yutaochen@aliyun.com>
2020-02-01 22:38:52 +0000

    Homepage layout

--- app.js
+++ app.js
@@ -15,9 +15,17 @@ app.get('/', function(req, res) {
     res.set({
         'Access-Control-Allow-Origin': '*'
     });
-    res.render('test', {title: 'Homepage'});
+    res.render('welcome_page', {title: 'Welcome to Merge.AI!'});
 }).listen(process.env.PORT || port, function() {
     console.log("Server listening at port " + port);
 });
 
+//render sign up page
+app.get('/sign_up_page', function(req, res) {
+    res.render('sign_up_page', {title: 'Sign up'});
+});
 
+//render login page
+app.get('/login_page', function(req, res) {
+    res.render('login_page', {title: 'Login'});
+});
