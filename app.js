160c7dd4571f4946f3cbd3b74a890afc2729689e
Yutao Chen <yutaochen@aliyun.com>
2020-04-12 15:05:55 +0100

    Account info and functions

--- app.js
+++ app.js
@@ -129,6 +129,13 @@ app.post('/login', function(req, res) {
 
 //render main activity page
 app.get('/main_activity', function(req, res) {
+    // Testing python script
+    const spawn = require("child_process").spawn;
+    const pythonProcess = spawn('python',["C:/My Stuff/MyCodes/Final Year Project/merge.ai/python/test.py"]);
+    pythonProcess.stdout.on('data', (data) => {
+        // Do something with the data returned from python script
+        console.log(data.toString());
+    });
     res.render('main_activity', {
         title: "Merge.AI",
         profile_photo_top_left: req.session.profile_photo_data,
@@ -136,9 +143,134 @@ app.get('/main_activity', function(req, res) {
     });
 });
 
+//render account info page
+app.get('/account_info', function(req, res) {
+    res.render('account_info', {
+        title: "Account Info",
+        profile_photo_top_left: req.session.profile_photo_data,
+        profile_photo_content_type_top_left: req.session.profile_photo_content_type,
+        userID: req.session.email,
+        username: req.session.username,
+        profile_photo: req.session.profile_photo_data,
+        profile_photo_content_type: req.session.profile_photo_content_type
+    });
+});
+
+//render change username page
+app.get('/change_username_page', function(req, res) {
+    res.render('change_username_page', {
+        title: 'Change username',
+
+        profile_photo_content_type_top_left: req.session.profile_photo_content_type,
+        profile_photo_top_left: req.session.profile_photo_data
+    });
+});
+
+//handle change username request
+app.post('/change_username', function(req, res) {
+
+    let username = req.body.username_change;
+
+    db.collection('Accounts').findOne({email: req.session.email}, function (err, result) {
+        if (err) throw err;
+        if (username === result.username) {
+            res.render('change_username_result',
+                {title: 'Username change failed!',
+                    message: 'Please use different username.',
+
+                    profile_photo_content_type_top_left: req.session.profile_photo_content_type,
+                    profile_photo_top_left: req.session.profile_photo_data
+                });
+            console.log("Same username, failed to commit");
+        } else {
+            db.collection("Accounts").updateOne({email: req.session.email},
+                {$set: {username: username}}, function(err) {
+                    if (err) throw err;
+                });
+            req.session.username = username;
+            res.render('change_username_result',
+                {title: 'Username change successful!',
+                    message: 'Click Return to go back.',
+
+                    profile_photo_content_type_top_left: req.session.profile_photo_content_type,
+                    profile_photo_top_left: req.session.profile_photo_data
+                });
+            console.log("Username change successful");
+        }
+    });
+});
+
+//render change password page
+app.get('/change_password_page', function(req, res) {
+    res.render('change_password_page', {
+        title: 'Change password',
+
+        profile_photo_content_type_top_left: req.session.profile_photo_content_type,
+        profile_photo_top_left: req.session.profile_photo_data
+    });
+});
+
+//handle change password request
+app.post('/change_password', function(req, res) {
+
+    let password = req.body.password_change;
+    //hash password before comparison
+    password = crypto.createHmac('sha1', password).update(password).digest('hex');
+
+    db.collection('Accounts').findOne({email: req.session.email}, function (err, result) {
+        if (err) throw err;
+        if (password === result.password) {
+            res.render('change_password_result',
+                {title: 'Password change failed!',
+                    message: 'Please use different password.',
+                    button_action: 'javascript:history.back()',
+                    button_value: 'Return',
+
+                    profile_photo_content_type_top_left: req.session.profile_photo_content_type,
+                    profile_photo_top_left: req.session.profile_photo_data
+                });
+            console.log("Same password, failed to commit");
+        } else {
+            db.collection("Accounts").updateOne({email: req.session.email},
+                {$set: {password: password}}, function(err) {
+                    if (err) throw err;
+                });
+            res.render('change_password_result',
+                {title: 'Password change successful!',
+                    message: 'Please re-login to your account.',
+                    button_action: '/logout',
+                    button_value: 'Logout',
+
+                    profile_photo_content_type_top_left: req.session.profile_photo_content_type,
+                    profile_photo_top_left: req.session.profile_photo_data
+                });
+            console.log("Password change successful");
+        }
+    });
+});
+
+//render delete account confirm page
+app.get('/delete_account_confirm', function (req, res) {
+    res.render('delete_account_confirm', {
+        title: 'Confirm before you go',
+
+        profile_photo_content_type_top_left: req.session.profile_photo_content_type,
+        profile_photo_top_left: req.session.profile_photo_data
+    });
+});
+
+//handle delete account request
+app.get('/delete_account', function(req, res) {
+    db.collection('Accounts').deleteOne({email: req.session.email}, function(err) {
+        if (err) throw err;
+    });
+    console.log('Account delete successful, user ID: ' + req.session.email);
+    res.redirect('/logout');
+});
+
 //handle logout request
 app.get('/logout', function(req, res) {
+    console.log('Logout successful, user ID: ' + req.session.email);
     req.session.destroy();
-    console.log('Logout successful');
     res.redirect('/');
 });
